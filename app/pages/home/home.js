583d18c901a4b065269009d13214d740caba81d0
Jop Bouckaert <jop.bouckaert@outlook.com>
2019-10-01 15:51:13 +0200

    add contact-us and repos page

--- app/pages/home/home.js
+++ app/pages/home/home.js
@@ -1,6 +1,18 @@
 const frameModule = require("ui/frame");
 
-exports.onAboutTap = function() {
-    console.log("Going to about");
-    frameModule.topmost().navigate("pages/about/about");
+let navigate = function(page) {
+    console.log(`Going to ${page}`);
+    frameModule.topmost().navigate(`pages/${page}/${page}`);
 };
+
+exports.onAboutTap = () => navigate("about");
+exports.onContactTap = () => navigate("contact-us");
+exports.onReposTap = () => navigate("repos");
+
+// exports.onAboutTap = function() {
+//     navigate("about");
+// };
+
+// exports.onContact = function() {
+//     navigate("contact-us");
+// };
