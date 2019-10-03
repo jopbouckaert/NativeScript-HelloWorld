0568a6e80319c315f052b5525e797d99249062bd
Jop Bouckaert <jop.bouckaert@outlook.com>
2019-10-03 16:57:12 +0200

    add github api

--- app/pages/profile/profile.js
+++ app/pages/profile/profile.js
@@ -1,14 +1,35 @@
 const observable = require("data/observable");
+const axios = require("axios");
 
 let user = observable.fromObject({
     id: "jopbouckaert",
     name: "unknown",
     nickname: "unspecified",
     repositories: "unknown",
-    followers: "unknown"
+    followers: "unknown",
+    avatar: ""
 });
 
-exports.onLoaded = function(args) {
+exports.onLoaded = function (args) {
     let page = args.object;
     page.bindingContext = user;
+
+    axios.get("https://api.github.com/users/" + user.id)
+        .then(function (response) {
+            user.name = response.data.name;
+            user.nickname = response.data.login;
+            user.repositories = response.data.public_repos;
+            user.followers = response.data.followers;
+            user.avatar = response.data.avatar_url;
+            console.log(response);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .finally(function () {
+            // always executed
+        });
 };
+
+// Make a request for a user with a given ID
